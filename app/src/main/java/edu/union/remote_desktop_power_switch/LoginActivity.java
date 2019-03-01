75e80f11a4fbc342eb5eb157c1bd50f7684c8262
Brian Zick <brian.a.zick@gmail.com>
2019-03-01 15:24:11 -0500

    update UI to work with theme

--- app/src/main/java/edu/union/remote_desktop_power_switch/LoginActivity.java
+++ app/src/main/java/edu/union/remote_desktop_power_switch/LoginActivity.java
@@ -1,22 +1,21 @@
 package edu.union.remote_desktop_power_switch;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity  extends AppCompatActivity {
+    private static final int COUNTER_START_VALUE = 3;
+
     Button b1,b2;
     EditText ed1,ed2;
-
     TextView tx1;
-    int counter = 3;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,32 +23,33 @@ public class LoginActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         b1 = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+
         ed1 = (EditText) findViewById(R.id.editText);
         ed2 = (EditText) findViewById(R.id.editText2);
 
-        b2 = (Button) findViewById(R.id.button2);
+        counter = COUNTER_START_VALUE;
+
         tx1 = (TextView) findViewById(R.id.textView3);
-        tx1.setVisibility(View.GONE);
+        tx1.setBackgroundColor(getResources().getColor(R.color.green));
+        tx1.setText(Integer.toString(counter));
     }
 
    public void attemptLogin(View view) {
         if(ed1.getText().toString().equals("admin") && ed2.getText().toString().equals("admin")) {
-            Toast.makeText(getApplicationContext(),"Redirecting...",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),getString(R.string.redirecting),Toast.LENGTH_SHORT).show();
             this.transitionToMainActivity(view);
         } else {
-            Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
-            tx1.setVisibility(View.VISIBLE);
-            tx1.setBackgroundColor(Color.RED);
+            Toast.makeText(getApplicationContext(), getString(R.string.wrong_credentials),Toast.LENGTH_SHORT).show();
             counter--;
             tx1.setText(Integer.toString(counter));
-
-            if (counter == 0) {
+            if (counter == 2) {
+                tx1.setBackgroundColor(getResources().getColor(R.color.red));
+            } else if (counter == 0) {
                 b1.setEnabled(false);
             }
         }
     }
-
-
 
     public void cancel(View view) {
         finish();
