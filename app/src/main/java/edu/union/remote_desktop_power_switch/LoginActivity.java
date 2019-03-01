b06d826e08ed618464d957bc0ad77dbd396c5c7b
Brian Zick <brian.a.zick@gmail.com>
2019-03-01 16:13:55 -0500

    complete implementation of username and password including login attempts limit and timeout

--- app/src/main/java/edu/union/remote_desktop_power_switch/LoginActivity.java
+++ app/src/main/java/edu/union/remote_desktop_power_switch/LoginActivity.java
@@ -1,7 +1,10 @@
 package edu.union.remote_desktop_power_switch;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -11,43 +14,44 @@ import android.widget.Toast;
 
 public class LoginActivity  extends AppCompatActivity {
     private static final int COUNTER_START_VALUE = 3;
+    private static final int TIME_OUT_IN_MINUTES = 5;
+    private static final int TIME_OUT_IN_MILLISECONDS = 60000;//300000;
 
-    Button b1,b2;
-    EditText ed1,ed2;
-    TextView tx1;
-    int counter;
+
+    private Button login_button, cancel_button;
+    private EditText username_field, password_field;
+    private TextView attempts_counter_display;
+    private int login_counter;
+    private SharedPreferences prefs;
+    private long timeLeft;
+    private CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        b1 = (Button) findViewById(R.id.button);
-        b2 = (Button) findViewById(R.id.button2);
+        login_button = (Button) findViewById(R.id.button);
+        cancel_button = (Button) findViewById(R.id.button2);
 
-        ed1 = (EditText) findViewById(R.id.editText);
-        ed2 = (EditText) findViewById(R.id.editText2);
+        username_field = (EditText) findViewById(R.id.editText);
+        password_field = (EditText) findViewById(R.id.editText2);
 
-        counter = COUNTER_START_VALUE;
+        attempts_counter_display = (TextView) findViewById(R.id.textView3);
+        reset_attempt_counter();
 
-        tx1 = (TextView) findViewById(R.id.textView3);
-        tx1.setBackgroundColor(getResources().getColor(R.color.green));
-        tx1.setText(Integer.toString(counter));
+        initTasks();
+        checkTimer();
     }
 
    public void attemptLogin(View view) {
-        if(ed1.getText().toString().equals("admin") && ed2.getText().toString().equals("admin")) {
+        if(username_field.getText().toString().equals("admin") && password_field.getText().toString().equals("admin")) {
             Toast.makeText(getApplicationContext(),getString(R.string.redirecting),Toast.LENGTH_SHORT).show();
             this.transitionToMainActivity(view);
         } else {
-            Toast.makeText(getApplicationContext(), getString(R.string.wrong_credentials),Toast.LENGTH_SHORT).show();
-            counter--;
-            tx1.setText(Integer.toString(counter));
-            if (counter == 2) {
-                tx1.setBackgroundColor(getResources().getColor(R.color.red));
-            } else if (counter == 0) {
-                b1.setEnabled(false);
-            }
+            login_counter--;
+            check_login_attempts();
+            update_attempt_counter();
         }
     }
 
@@ -58,5 +62,84 @@ public class LoginActivity  extends AppCompatActivity {
     public void transitionToMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    private void initTasks() {
+       prefs = getSharedPreferences("file", Context.MODE_PRIVATE);
+    }
+
+    private void checkTimer() {
+        if (prefs.contains("time"))
+            setTimer();
+        else {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putLong("time", -1L);
+            editor.apply();
+        }
+    }
+
+    private void setTimer() {
+        timeLeft = prefs.getLong("time", -1L);
+        if (timeLeft != -1L)
+            startTimer(timeLeft);
+        else
+            login_button.setEnabled(true);
+
+    }
+
+    private void startTimer(long time) {
+        login_button.setEnabled(false);
+        timer = new CountDownTimer(time, 1000) {
+
+            @Override
+            public void onFinish() {
+                reset_attempt_counter();
+                login_button.setEnabled(true);
+                saveToPref(-1L);
+            }
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //update UI, if required
+                timeLeft = millisUntilFinished;
+                saveToPref(timeLeft);
+            }
+        }.start();
+    }
+
+    private void saveToPref(long timeLeft){
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putLong("time", timeLeft);
+        editor.apply();
+    }
+
+    private void reset_attempt_counter(){
+        login_counter = COUNTER_START_VALUE;
+        update_attempt_counter();
+    }
+
+    private void check_login_attempts(){
+        if (login_counter <= 0) {
+            Toast.makeText(getApplicationContext(), getString(R.string.timeout, TIME_OUT_IN_MINUTES),Toast.LENGTH_SHORT).show();
+            this.startTimer(TIME_OUT_IN_MILLISECONDS);
+        } else {
+            Toast.makeText(getApplicationContext(), getString(R.string.wrong_credentials),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void update_attempt_counter(){
+        if (login_counter <= 2 && login_counter >= 0) {
+            attempts_counter_display.setBackgroundColor(getResources().getColor(R.color.red));
+        } else if (login_counter >= 3) {
+            attempts_counter_display.setBackgroundColor(getResources().getColor(R.color.green));
+        }
+        attempts_counter_display.setText(Integer.toString(login_counter));
+    }
+
+    protected void onResume() {
+        super.onResume();
+        password_field.setText("");
+        username_field.setText("");
+        reset_attempt_counter();
     }
 }
